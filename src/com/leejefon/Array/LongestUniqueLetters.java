baea231605895ec092667711d05dc8a3cc72d84b
Jeff Lee <jeff@healthiq.com>
2017-08-18 00:08:46 -0700

    Initial commit with basic project setup

---
+++ src/com/leejefon/Array/LongestUniqueLetters.java
@@ -0,0 +1,10 @@
+package com.leejefon.Array;
+
+/**
+ * Created by jefflee on 8/17/17.
+ */
+public class LongestUniqueLetters {
+    public static int main() {
+        return 3;
+    }
+}
---
+++ test/com/leejefon/Array/LongestUniqueLettersTest.java
@@ -0,0 +1,12 @@
+package com.leejefon.Array;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jefflee on 8/18/17.
+ */
+public class LongestUniqueLettersTest extends TestCase {
+    public void testString() throws Exception {
+        assertNotNull(true);
+    }
+}
